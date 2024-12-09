6ef130d8caac7d3154bee6d3328dafaccd27bddc
Isaac Lozano <109lozanoi@gmail.com>
2024-12-08 19:29:57 -0800

    search_set: Revamp and add new features
    Added getopts support for nice commandline usage. Also added begin and
    end options for limiting the range of the reported output.

--- src/bin/search_set.rs
+++ src/bin/search_set.rs
@@ -1,7 +1,12 @@
 use std::env;
+use std::error::Error;
 use std::fs::File;
 use std::marker::PhantomData;
+use std::num::ParseIntError;
+use std::process;
 use std::u16;
+
+use getopts::Options;
 
 use sa2_piece_gen::rng::Rng;
 use sa2_piece_gen::emerald_manager::EmeraldManager;
@@ -51,63 +56,122 @@ enum PieceConstraint {
 }
 
 impl PieceConstraint {
-    fn from_str(s: &str) -> PieceConstraint {
+    fn from_str(s: &str) -> Result<PieceConstraint, ParseIntError> {
         if s.starts_with('G') {
-            PieceConstraint::GrabbedId(u16::from_str_radix(s.strip_prefix('G').unwrap(), 16).unwrap())
+            Ok(PieceConstraint::GrabbedId(u16::from_str_radix(s.strip_prefix('G').unwrap(), 16)?))
         }
         else if s == "X" {
-            PieceConstraint::DontCare
+            Ok(PieceConstraint::DontCare)
         }
         else {
-            PieceConstraint::Want(u16::from_str_radix(&s, 16).unwrap())
+            Ok(PieceConstraint::Want(u16::from_str_radix(&s, 16)?))
         }
     }
 }
 
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage {} -p PLATFORM -s STAGE [OPTIONS] P1 P2 P3", program);
+    print!("{}", opts.usage(&brief));
+}
+
 fn main() {
-    let mut args = env::args().skip(1);
-    let platform = args.next().unwrap();
-    let input_filename = args.next().unwrap();
-    let p1_string = args.next().unwrap();
-    let p2_string = args.next().unwrap();
-    let p3_string = args.next().unwrap();
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].as_str();
 
-    let p1_id = PieceConstraint::from_str(&p1_string);
-    let p2_id = PieceConstraint::from_str(&p2_string);
-    let p3_id = PieceConstraint::from_str(&p3_string);
+    let mut opts = Options::new();
+    opts.reqopt("p", "platform", "set platform to simulate", "PLATFORM");
+    opts.reqopt("s", "stage", "set stage-spec file", "STAGE");
+    opts.optopt("b", "begin", "set initial RNG call amount (default 0)", "RNG_CALLS");
+    opts.optopt("e", "end", "set final RNG call amount (default infinity)", "RNG_CALLS");
+    opts.optflag("h", "help", "print this help menu");
 
-    let input = File::open(input_filename).unwrap();
-    let spec: StageSpec = serde_json::from_reader(input).unwrap();
+    let matches = opts.parse(&args[1..]).expect("Could not parse arguments");
+
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+
+    let platform = matches.opt_str("p").unwrap();
+    let input_filename = matches.opt_str("s").unwrap();
+    let begin = matches.opt_get("b").expect("Error parsing begin value");
+    let end = matches.opt_get("e").expect("Error parsing end value");
+
+    if matches.free.len() != 3 {
+        panic!("Incorrect number of piece descriptors (must be 3)");
+    }
+
+    let p1_string = &matches.free[0];
+    let p2_string = &matches.free[1];
+    let p3_string = &matches.free[2];
+
+    let p1_id = PieceConstraint::from_str(p1_string).expect("Error parsing piece 1");
+    let p2_id = PieceConstraint::from_str(p2_string).expect("Error parsing piece 2");
+    let p3_id = PieceConstraint::from_str(p3_string).expect("Error parsing piece 3");
+
+    let input = File::open(input_filename).expect("Error opening stage-spec file");
+    let spec: StageSpec = serde_json::from_reader(input).expect("Error reading stage-spec file");
 
     match platform.as_str() {
-        "pc" => piece_sequence::<Pc>(spec, p1_id, p2_id, p3_id),
-        "gc" => piece_sequence::<Gc>(spec, p1_id, p2_id, p3_id),
+        "pc" => piece_sequence::<Pc>(spec, begin, end, p1_id, p2_id, p3_id),
+        "gc" => piece_sequence::<Gc>(spec, begin, end, p1_id, p2_id, p3_id),
         _ => unimplemented!(),
     }
 }
 
-fn piece_sequence<P>(spec: StageSpec, p1: PieceConstraint, p2: PieceConstraint, p3: PieceConstraint)
+fn piece_sequence<P>(spec: StageSpec, begin: Option<u32>, end: Option<u32>, p1: PieceConstraint, p2: PieceConstraint, p3: PieceConstraint)
     where P: Platform,
 {
-    let r_iter: RngIterator<P> = RngIterator::new(0);
+    let begin = begin.unwrap_or(0);
+    let r_iter: RngIterator<P> = RngIterator::new(begin);
 
     for (idx, r) in r_iter.enumerate() {
+        if let Some(end_val) = end {
+            if begin + idx as u32 == end_val {
+                return;
+            }
+        }
+
         let r_copy = r;
         let mut em = EmeraldManager::from_spec::<P>(spec.clone());
 
-        if let PieceConstraint::GrabbedId(id) = p1 {
-            em.p1 = spec.get_emerald_by_id(id).unwrap();
-            em.p1.id = 0xFE00;
+        match p1 {
+            PieceConstraint::Want(id) => {
+                if spec.get_emerald_by_id(id).is_none() {
+                    panic!("Invalid p1 ID (not present in stage)");
+                }
+            },
+            PieceConstraint::GrabbedId(id) => {
+                em.p1 = spec.get_emerald_by_id(id).expect("Invalid p1 ID (not present in stage)");
+                em.p1.id = 0xFE00;
+            },
+            _ => {}
         }
 
-        if let PieceConstraint::GrabbedId(id) = p2 {
-            em.p2 = spec.get_emerald_by_id(id).unwrap();
-            em.p2.id = 0xFE00;
+        match p2 {
+            PieceConstraint::Want(id) => {
+                if spec.get_emerald_by_id(id).is_none() {
+                    panic!("Invalid p2 ID (not present in stage)");
+                }
+            },
+            PieceConstraint::GrabbedId(id) => {
+                em.p2 = spec.get_emerald_by_id(id).expect("Invalid p2 ID (not present in stage)");
+                em.p2.id = 0xFE00;
+            },
+            _ => {}
         }
 
-        if let PieceConstraint::GrabbedId(id) = p3 {
-            em.p3 = spec.get_emerald_by_id(id).unwrap();
-            em.p3.id = 0xFE00;
+        match p3 {
+            PieceConstraint::Want(id) => {
+                if spec.get_emerald_by_id(id).is_none() {
+                    panic!("Invalid p3 ID (not present in stage)");
+                }
+            },
+            PieceConstraint::GrabbedId(id) => {
+                em.p3 = spec.get_emerald_by_id(id).expect("Invalid p3 ID (not present in stage)");
+                em.p3.id = 0xFE00;
+            },
+            _ => {}
         }
 
         em.r = r;
@@ -128,7 +192,7 @@ fn piece_sequence<P>(spec: StageSpec, p1: PieceConstraint, p2: PieceConstraint, 
         }
 
         if matched {
-            println!("{:04}: {:04X} {:04X} {:04X}", idx, em.p1.id, em.p2.id, em.p3.id);
+            println!("{}: {:04X} {:04X} {:04X}", begin + idx as u32, em.p1.id, em.p2.id, em.p3.id);
             println!("Rng state: 0x{:08X}", r_copy.get_state());
             println!();
         }
